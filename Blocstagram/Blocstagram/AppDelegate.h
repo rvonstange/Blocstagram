64316708fdcb63b715bd305adf72851640965a36
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-10-07 15:21:54 -0400

    initial commit w/storyboard gone

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Robert von Stange on 10/7/15.
+//  Copyright (c) 2015 Robert von Stange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Robert von Stange on 10/7/15.
+//  Copyright (c) 2015 Robert von Stange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
